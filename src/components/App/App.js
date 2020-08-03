e7ad31959c7f214bcfa480922c8473f818e65147
Natalya Myunster <natalyamyunster@yahoo.com>
2020-08-03 07:19:18 +0200

    added initialState in App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {TodoList} from '../TodoList/TodoList';
+import { v4 as uuid } from 'uuidv4';
 
 import s from  './App.module.css';
 
 function App() {
+    const initialState = [
+        {
+            todo: 'Watch the last lesson\'s record',
+            isDone: false,
+            id: uuid
+        }, {
+            todo: 'Make homework',
+            isDone: false,
+            id: uuid
+        }, {
+            todo: 'Read the documentation',
+            isDone: false,
+            id: uuid
+        }
+    ];
+    const [todos, setTodos] = useState(initialState);
+
+    const todoListJSX = todos.map((el, i)=>(
+        <TodoList key={el.id} idx={i} todo={el} />
+    ));
+
   return (
     <div className={s.App}>
       <h1>My TO-DO List</h1>
-      <TodoList />
+        {todoListJSX}
     </div>
   );
 }
